0417413c7d17b9c87a30636e338f991be4b91957
Burrows Wang <burrowswang@gmail.com>
2016-08-15 23:57:16 +0800

    ChocolatesByNumbers

---
+++ Codility/ChocolatesByNumbers.h
@@ -0,0 +1,13 @@
+//
+//  ChocolatesByNumbers.h
+//  Codility
+//
+//  Created by Burrows on 16/8/15.
+//  Copyright © 2016年 burrowswang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChocolatesByNumbers : NSObject
+
+@end
